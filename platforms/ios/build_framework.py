881ef24f13fec9363a0addbab6fd6f5eba8d16fc
Viral Savani <viral.savani@image-metrics.com>
2018-07-17 20:12:11 -0700

    add ImageMetrics cmake options

--- platforms/ios/build_framework.py
+++ platforms/ios/build_framework.py
@@ -27,7 +27,7 @@ However, opencv2.framework directory is erased and recreated on each run.
 
 import glob, re, os, os.path, shutil, string, sys
 
-def build_opencv(srcroot, buildroot, target, arch):
+def build_opencv(srcroot, buildroot, target, arch, cmake_options):
     "builds OpenCV for device or simulator"
 
     builddir = os.path.join(buildroot, target + '-' + arch)
@@ -37,10 +37,9 @@ def build_opencv(srcroot, buildroot, target, arch):
     os.chdir(builddir)
     # for some reason, if you do not specify CMAKE_BUILD_TYPE, it puts libs to "RELEASE" rather than "Release"
     cmakeargs = ("-GXcode " +
+                cmake_options +
                 "-DCMAKE_BUILD_TYPE=Release " +
                 "-DCMAKE_TOOLCHAIN_FILE=%s/platforms/ios/cmake/Toolchains/Toolchain-%s_Xcode.cmake " +
-                "-DBUILD_opencv_world=ON " +
-                "-DCMAKE_C_FLAGS=\"-Wno-implicit-function-declaration\" " +
                 "-DCMAKE_INSTALL_PREFIX=install") % (srcroot, target)
     # if cmake cache exists, just rerun cmake to update OpenCV.xproj if necessary
     if os.path.isfile(os.path.join(builddir, "CMakeCache.txt")):
@@ -94,20 +93,65 @@ def put_framework_together(srcroot, dstroot):
     os.symlink("Versions/Current/opencv2", "opencv2")
 
 
-def build_framework(srcroot, dstroot):
+def build_framework(srcroot, dstroot, cmake_options):
     "main function to do all the work"
 
     targets = ["iPhoneOS", "iPhoneOS", "iPhoneOS", "iPhoneSimulator", "iPhoneSimulator"]
     archs = ["armv7", "armv7s", "arm64", "i386", "x86_64"]
     for i in range(len(targets)):
-        build_opencv(srcroot, os.path.join(dstroot, "build"), targets[i], archs[i])
+        build_opencv(srcroot, os.path.join(dstroot, "build"), targets[i], archs[i], cmake_options)
 
     put_framework_together(srcroot, dstroot)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage:\n\t./build_framework.py <outputdir>\n\n"
+    if len(sys.argv) != 3:
+        print "Usage:\n\t./build_framework.py <outputdir> <path_to_eigen_third_party>\n\n"
         sys.exit(0)
 
-    build_framework(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "../..")), os.path.abspath(sys.argv[1]))
+    cmake_options = (
+        "-DBUILD_opencv_world=ON " +
+        "-DAPPLE_FRAMEWORK=ON " +
+        "-DBUILD_EXAMPLES=NO " +
+        "-DBUILD_TESTS=NO " +
+        "-DBUILD_NEW_PYTHON_SUPPORT=NO " +
+        "-DBUILD_PERF_TESTS=NO " +
+        "-DBUILD_SHARED_LIBS=NO " +
+        "-DBUILD_JPEG=NO " +
+        "-DBUILD_OPENEXR=NO " +
+        "-DBUILD_PNG=NO " +
+        "-DBUILD_TIFF=NO " +
+        "-DBUILD_JASPER=NO " +
+        "-DBUILD_TBB=NO " +
+        "-DBUILD_opencv_gpu=NO " +
+        "-DBUILD_opencv_java=NO " +
+        "-DBUILD_opencv_androidcamera=NO " +
+        "-DWITH_OPENNI=OFF " +
+        "-DWITH_OPENGL=OFF " +
+        "-DWITH_IMAGEIO=ON " +
+        "-DWITH_NVCUVID=OFF " +
+        "-DWITH_CUFFT=OFF " +
+        "-DWITH_CUBLAS=OFF " +
+        "-DWITH_CUDA=OFF " +
+        "-DWITH_HALIDE=OFF " +
+        "-DWITH_OPENCL=OFF " +
+        "-DWITH_OPENEXR=OFF " +
+        "-DWITH_JPEG=OFF " +
+        "-DWITH_PNG=OFF " +
+        "-DWITH_CAROTENE=OFF " +
+        "-DWITH_WEBP=OFF " +
+        "-DWITH_TIFF=OFF " +
+        "-DWITH_JASPER=OFF " +
+        "-DWITH_IPP=OFF " +
+        "-DWITH_ITT=OFF " +
+        "-DWITH_TBB=OFF " +
+        "-DWITH_EIGEN=YES " +
+        "-DWITH_AVFOUNDATION=OFF " +
+        "-DENABLE_DYNAMIC_CUDA=OFF " +
+        "-DCUDA_HOST_COMPILATION_CPP=OFF " +
+        "-DEIGEN_INCLUDE_PATH="+sys.argv[2] +" " +
+        "-DCMAKE_C_FLAGS=\"-Wno-implicit-function-declaration -Wno-error -Wno-tautological-pointer-compare -Wno-tautological-compare -Wno-pointer-bool-conversion\"  " +
+        "-DCMAKE_CXX_FLAGS=\"-fvisibility=hidden -fvisibility-inlines-hidden -Wno-error -Wno-tautological-pointer-compare -Wno-tautological-compare -Wno-pointer-bool-conversion -Os\"  " +
+        "-DCMAKE_SHARED_LINKER_FLAGS=\"-Wl,--gc-sections\"  ")
+
+    build_framework(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "../..")), os.path.abspath(sys.argv[1]), cmake_options)
